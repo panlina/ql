bbb415606e200dd7d4ae2e93020bf6d9c2dba713
Jack Lu <jacklu@jacklu.me>
2019-06-30 16:24:55 +0800

    +parentheses

--- semantics.js
+++ semantics.js
@@ -3,6 +3,8 @@ var Expression = require('./Expression');
 var semantics = grammar.createSemantics().addOperation('parse', {
 	identifier: x => x.sourceString,
 	ExpressionName: identifier => new Expression.Name(identifier.parse()),
+	ExpressionAtom: expression => expression.parse(),
+	ExpressionAtom_parentheses: (open, expression, close) => expression.parse(),
 	ExpressionFilter: expression => expression.parse(),
 	ExpressionFilter_filter: (expression, bar, filter) => new Expression.Filter(
 		expression.parse(),
