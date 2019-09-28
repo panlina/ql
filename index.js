43384c13e876f108023bdad1eb7851e4aeee642f
Jack Lu <jacklu@jacklu.me>
2019-09-29 00:38:04 +0800

    export Expression

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.parse = require('./parse');
 exports.compile = require('./compile');
 exports.Environment = require('./Environment');
 exports.Scope = require('./Scope');
+exports.Expression = require('./Expression');
