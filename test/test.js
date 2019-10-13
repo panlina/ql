ebe726fd494f1db6eb97cfe0094d4feab8e399d2
Jack Lu <jacklu@jacklu.me>
2019-10-13 15:14:22 +0800

    test: type->local

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var ql = require('..');
 var data, type = require('./type');
 type = require('lodash.mapvalues')(type, require('../Type.parse'));
-type = require('lodash.transform')(type, (result, value, key) => {
+var local = require('lodash.transform')(type, (result, value, key) => {
 	result[`${key}s`] = [value];
 });
 before(function () {
@@ -27,8 +27,8 @@ before(function () {
 });
 it('', function () {
 	var q = ql.parse("posts|!this.id>50&&(t=::posts#1.title,title<=t)");
-	var _function = ql.compile.call(new ql.Environment(new ql.Scope(type)), q);
-	assert(require('../Type.equals')(_function.type, type.posts));
+	var _function = ql.compile.call(new ql.Environment(new ql.Scope(local)), q);
+	assert(require('../Type.equals')(_function.type, [type.post]));
 	assert.equal(
 		_function.call(new ql.Environment(new ql.Scope(data))).length,
 		data.posts.filter(post => !(post.id > 50) && post.title <= data.posts.find(post => post.id == 1).title).length
@@ -36,8 +36,8 @@ it('', function () {
 });
 it('', function () {
 	var q = ql.parse("users|(posts|id>10)");
-	var _function = ql.compile.call(new ql.Environment(new ql.Scope(type)), q);
-	assert(require('../Type.equals')(_function.type, type.users));
+	var _function = ql.compile.call(new ql.Environment(new ql.Scope(local)), q);
+	assert(require('../Type.equals')(_function.type, [type.user]));
 	assert.equal(
 		_function.call(new ql.Environment(new ql.Scope(data))).length,
 		data.users.filter(user => data.posts.filter(post => post.userId == user.id && post.id > 10).length > 0).length
