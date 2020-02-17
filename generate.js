33a31c8a2a44c4081531e35dd3c5fd8cd19b5bd7
Jack Lu <jacklu@jacklu.me>
2020-02-17 23:56:56 +0800

    update generate

--- generate.js
+++ generate.js
@@ -10,19 +10,25 @@ function generate(expression) {
 			return `{${expression.property.map(property => `${property.name}:${generate(property.value)}`).join(',')}}`;
 		case 'array':
 			return `[${expression.element.map(element => generate(element)).join(',')}]`;
+		case 'tuple':
+			return `{${expression.element.map(
+				element => {
+					var $element = generate(element);
+					if (element.type == 'comma')
+						$element = `(${$element})`
+					return $element;
+				}
+			).join(',')}}`;
+		case 'id':
+			var id = generate(expression.id);
+			if (precedence[expression.id.type] >= precedence[expression.type])
+				id = `(${id})`;
+			return `${expression.identifier}#${id}`;
 		case 'property':
 			var $expression = generate(expression.expression);
 			if (precedence[expression.expression.type] > precedence[expression.type])
 				$expression = `(${$expression})`;
 			return `${$expression}.${expression.property}`;
-		case 'index':
-			var $expression = generate(expression.expression);
-			if (precedence[expression.expression.type] > precedence[expression.type])
-				$expression = `(${$expression})`;
-			var index = generate(expression.index);
-			if (precedence[expression.index.type] >= precedence[expression.type])
-				index = `(${index})`;
-			return `${$expression}#${index}`;
 		case 'call':
 			var $expression = generate(expression.expression);
 			if (precedence[expression.expression.type] > precedence[expression.type])
@@ -65,6 +71,17 @@ function generate(expression) {
 					!!expression.right
 				).precedence;
 			}
+		case 'conditional':
+			var $condition = generate(expression.condition);
+			if (precedence[expression.condition.type] > precedence[expression.type])
+				$condition = `(${$condition})`;
+			var $true = generate(expression.true);
+			if (precedence[expression.true.type] > precedence[expression.type])
+				$true = `(${$true})`;
+			var $false = generate(expression.false);
+			if (precedence[expression.false.type] > precedence[expression.type])
+				$false = `(${$false})`;
+			return `${$condition}?${$true}:${$false}`;
 		case 'filter':
 			var $expression = generate(expression.expression);
 			if (precedence[expression.expression.type] > precedence[expression.type])
@@ -96,7 +113,9 @@ function generate(expression) {
 			var orderer = generate(expression.orderer);
 			if (precedence[expression.orderer.type] >= precedence[expression.type])
 				orderer = `(${orderer})`;
-			return `${$expression} order ${orderer}`;
+			if (expression.direction != undefined)
+				var $direction = ` ${{ false: "asc", true: "desc" }[expression.direction]}`;
+			return `${$expression} order ${orderer}${$direction || ''}`;
 		case 'group':
 			var $expression = generate(expression.expression);
 			if (precedence[expression.expression.type] > precedence[expression.type])
@@ -105,6 +124,11 @@ function generate(expression) {
 			if (precedence[expression.grouper.type] >= precedence[expression.type])
 				grouper = `(${grouper})`;
 			return `${$expression} group ${grouper}`;
+		case 'distinct':
+			var $expression = generate(expression.expression);
+			if (precedence[expression.expression.type] >= precedence[expression.type])
+				$expression = `(${$expression})`;
+			return `distinct ${$expression}`;
 		case 'comma':
 			var value = generate(expression.head.value);
 			if (precedence[expression.head.value.type] >= precedence[expression.type])
@@ -119,15 +143,17 @@ var precedence = {
 	literal: 0,
 	name: 0,
 	this: 0,
-	property: 1,
-	index: 1,
-	call: 2,
-	operation: 3,
-	filter: 4,
-	map: 4,
-	limit: 4,
-	order: 4,
-	group: 4,
-	comma: 5
+	id: 1,
+	property: 2,
+	call: 3,
+	operation: 4,
+	conditional: 5,
+	filter: 6,
+	map: 6,
+	limit: 6,
+	order: 6,
+	group: 6,
+	distinct: 6,
+	comma: 7
 };
 module.exports = generate;
