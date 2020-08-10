a8f7a671f37b89c15ebe377bf8623846965b745e
Jack Lu <jacklu@jacklu.me>
2020-08-10 23:31:15 +0800

    +ql.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import * as ql from './ql';
+export = ql;
 export * as Expression from './Expression';
 export * as Type from './Type';
 export * as Scope from './Scope';
---
+++ ql.d.ts
@@ -0,0 +1,3 @@
+import * as Expression from "./Expression";
+function ql(strings: string[], ...expressions: Expression[]): Expression;
+export = ql;
