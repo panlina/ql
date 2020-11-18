804c8bfba1d94070de4ac8ec769f2a5626c29e8f
Jack Lu <jacklu@jacklu.me>
2020-11-18 22:59:04 +0800

    fix | operator precedence

--- operator.js
+++ operator.js
@@ -14,7 +14,7 @@ var operator = [
 	{ name: 'in', left: true, right: true, precedence: 3 },
 	{ name: '!', left: false, right: true, precedence: 4 },
 	{ name: '&', left: true, right: true, precedence: 5 },
-	{ name: '|', left: true, right: true, precedence: 5 },
+	{ name: '|', left: true, right: true, precedence: 6 },
 	{ name: '#', left: true, right: false, precedence: -1 }
 ];
 var group = require('lodash.groupby')(operator, 'name');
